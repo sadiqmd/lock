2bbe99aec0a5c6ff2172f254e1df0d451fcdd227
Sadique Mohammed <sadiqmd@hotmail.com>
2018-03-08 18:07:32 +0530

    update

--- examples/bundling/browserify/index.js
+++ examples/bundling/browserify/index.js
@@ -1,7 +1,7 @@
-import Auth0Lock from 'auth0-lock';
+import Auth0Lock from '../../../src/auth0-lock';
 
-const cid = "yKJO1ckwuY1X8gPEhTRfhJXyObfiLxih";
-const domain = "mdocs.auth0.com";
+const cid = 'yKJO1ckwuY1X8gPEhTRfhJXyObfiLxih';
+const domain = 'mdocs.auth0.com';
 
 const lock = new Auth0Lock(cid, domain);
 
